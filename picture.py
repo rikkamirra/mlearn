6323f9552dcf17f517f34661ec078bcffb177daa
Masha <rikka1128@gmail.com>
2018-11-05 18:23:42 +0300

    Train on different digits

--- picture.py
+++ picture.py
@@ -1,4 +1,4 @@
-from mnist import MNIST
+from contextlib import contextmanager
 
 
 class Neuron:
@@ -11,19 +11,25 @@ class Neuron:
         self.size = self.x_size * self.y_size
         self.train_count = 0
         self.matrix = [[self.init_value for i in range(x_size)] for j in range(y_size)]
+        self.weight_matrix = [[self.get_weight(i, j) for i in range(x_size)] for j in range(y_size)]
+
+    def get_weight(self, i, j):
+        x_center = int(self.x_size / 2)
+        y_center = int(self.y_size / 2)
+        return (i / x_center) * (j / y_center)
 
     def train(self, input_matrix):
         self.validate_input(input_matrix)
         self.train_count += 1
-        with self.iterover() as i, j:
-            self.matrix[i][j] += input_matrix[i][j]
+        for i, j in self.iterover():
+            self.matrix[i][j] += input_matrix[i][j] * self.weight_matrix[i][j]
             self.matrix[i][j] /= self.train_count
 
     def recognize(self, input_matrix):
         p = 0
         self.validate_input(input_matrix)
-        with self.iterover() as i, j:
-            p += self.matrix[i][j] * input_matrix[i][j] / self.size
+        for i, j in self.iterover():
+            p += self.matrix[i][j] * input_matrix[i][j] * self.weight_matrix[i][j] / self.size
         return p
 
     def iterover(self):
@@ -33,17 +39,106 @@ class Neuron:
 
     def validate_input(self, input_matrix):
         if len(input_matrix) != self.y_size or len(input_matrix[0]) != self.x_size:
-            raise Exception("Invalid input size")
+            raise Exception(f'Invalid input size. Must be x = {self.x_size}, y = {self.y_size}. But have x = {len(input_matrix)} y = {len(input_matrix[0])}')
+
+    def show(self):
+        res = ''
+        for row in self.matrix:
+            res_ = ''
+            for item in row:
+                res_ += str(item)
+            res += res_ + '\n'
+        return res
 
 
-class DigitCollection:
-    def __init__(self, directory=''):
-        self.data = MNIST(directory)
-        self.images, self.labels = self.data.load_training()
-        print(self.data.display(self.images[0]))
-        for i in range(len(self.images)):
-            # print(self.data.display(self.images[i]))
-            print(self.data.display(self.labels))
+class Brain:
+    def __init__(self):
+        self.neurons = []
+
+    def add(self, neuron: Neuron):
+        self.neurons.append(neuron)
+
+    def recognize(self, input_string):
+        rerults = {}
+        input_matrix = self.build_matrix(input_string)
+        for neuron in self.neurons:
+            rerults[neuron.name] = neuron.recognize(input_matrix)
+        return rerults
+
+    @classmethod
+    def build_matrix(cls, string):
+        def convert(item):
+            if item == '@':
+                return 1
+            return 0
+
+        rows = string.split('\n')
+        matrix = []
+        for row in rows:
+            row_items = []
+            for i in row.strip():
+                row_items.append(convert(i))
+            if row_items:
+                matrix.append(row_items)
+        return matrix
+
+    @classmethod
+    def train_neuron(cls, neuron, trainfilename):
+        with open(trainfilename, 'r') as f:
+            train_staff = f.read()
+        train_digits = train_staff.split('***')
+        for train_digit in train_digits:
+            neuron.train(cls.build_matrix(train_digit))
+        return neuron
+
+def main():
+    X_SIZE = Y_SIZE = 28
+    digits = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+    neurons = dict((symbol, Neuron(symbol, X_SIZE, Y_SIZE)) for symbol in digits)
+
+    for digit in digits:
+        with open(f'digits/{digit}.digit', 'r') as f:
+            train_string = f.read()
+            Brain.train_neuron(neurons[digit], f'digits/{digit}.digit')
+
+    string = '''
+    ............................
+    ............................
+    ............................
+    ............................
+    ............................
+    ......................@.....
+    .....................@@.....
+    ....................@@......
+    ....................@.......
+    ...................@........
+    .............@....@.........
+    ............@....@@.........
+    ..........@@.....@..........
+    ..........@.....@...........
+    .........@@....@@...........
+    .........@@@@@@@@@@@........
+    ..............@.............
+    .............@@.............
+    .............@..............
+    ............@@..............
+    ............@...............
+    ...........@................
+    ...........@................
+    ..........@.................
+    ..........@.................
+    ............................
+    ............................
+    ............................
+    '''
+
+    brain = Brain()
+    for k, v in neurons.items():
+        brain.add(v)
+
+    print(brain.recognize(string))
+
+
 
 if __name__ == '__main__':
-    digits = DigitCollection()
+    main()
